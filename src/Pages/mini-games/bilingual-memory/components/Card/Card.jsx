612e4820a1b116d8ce9eed321d39ca2963ee5b8a
Sobhi Mansour <sobhi@Sobhis-MBP.lan>
2022-06-29 10:34:42 +0300

    mapping funcs

--- src/Pages/mini-games/bilingual-memory/components/Card/Card.jsx
+++ src/Pages/mini-games/bilingual-memory/components/Card/Card.jsx
@@ -1,12 +1,25 @@
 import "./Card.css";
+import React, { useEffect, useState } from "react";
+
 const Card = (props) => {
+  const [hidden, setHidden] = useState();
+  const [open, setOpen] = useState(false);
   return (
-    <div id="CardParent">
-      <div className="front"></div>
-      <div className="back">
-        <p className={props.isArabic ? `arabicCard` : null}>{props.text}</p>
-      </div>
-    </div>
-  );
-};
+
+    <div id="CardParent" onClick={()=>{
+      props.onClick(props.id); 
+      props.flips<2 ? setOpen(true) : setOpen(false);
+      }}>
+        
+    {  open  ? 
+    <div  className="back">
+        <p  className={ props.isArabic ? `arabicCard` : null }>{props.text}</p>
+      </div> 
+       :  
+       <div className="front">front</div>
+    
+}
+</div>
+)}
+
 export default Card;
--- src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
+++ src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
@@ -6,7 +6,10 @@ import { shuffleRandomly } from "../../../../../utils/utils";
 const GameBoardMemory = () => {
   const [currentId, setCurrentId] = useState();
   const [hidden, setHidden] = useState("hidden");
-  const [cardOpen, setCardOpen] = useState(false);
+  // const [firstFlip, setFirstFlip] = useState(true);
+  const [flips, setFlips] = useState(0);
+  const [wordsArrayState, setWordsArrayState] = useState(0);
+
 
   // randomly choose 6 word objects from data to fill 12 tiles
   let arrayOfWordObjects = [];
@@ -15,16 +18,15 @@ const GameBoardMemory = () => {
     let randomIndex = Math.floor(Math.random() * myWords.length);
     arrayOfWordObjects.push(myWords[randomIndex]);
     i++;
-    console.log(randomIndex)
   }
 
   //make array of {word & ID} pairs from hebrew and arabic and id
   let wordsArray = []
   arrayOfWordObjects.forEach((word) => {
-    // console.log(word);
       wordsArray.push({ word: word.hebrew, id: word.id },
                       { word: word.arabic, id: word.id, isArabic: true })
-    });
+                    });
+                    setWordsArrayState(...wordsArray)
 
   let shuffledWords = shuffleRandomly(wordsArray);
 
@@ -32,21 +34,36 @@ const GameBoardMemory = () => {
     return words.map((word, i) => {
       return (
         <div key={i}>
-          <Card text={word.word} id={word.id} isArabic={word.isArabic}/>
+          <Card
+           text={word.word} 
+           id={word.id} 
+           isArabic={word.isArabic} 
+           onClick={handleTileClick}
+           flips={flips}
+            />
         </div>
       );
     });
   };
-  const handleTileClick = (ev) => {
-    if (cardOpen) {
-      //do matching logic
-    } else {
+  const handleTileClick = (e) => {
+    if (flips<2) {
+      setFlips(prev => prev+1);
+       setCurrentId(e);
+       
       //save current card to state and wait
+    } else {
+        //do matching logic
+        e === currentId ?  console.log('handleRightAnswer and removecards') 
+        : console.log('close both cards');
+        setFlips(0);
     }
   };
   return <div className="GameBoardMemory">{drawBoard(shuffledWords)}</div>;
 };
 export default GameBoardMemory;
+
+
+
 
 const myWords = [
   {
@@ -54,41 +71,41 @@ const myWords = [
     arabic: "ااااا",
     Hspelling: "beit sefer", // Hebrew pronounciation in english letters
     Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: "som id here",
+    id: 2,
   },
   {
     hebrew: "בבבב",
     arabic: "بببب",
     Hspelling: "beit sefer", // Hebrew pronounciation in english letters
     Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: "som id here",
+    id: 3,
   },
   {
     hebrew: "גגגגג",
     arabic: "ججججج",
     Hspelling: "beit sefer", // Hebrew pronounciation in english letters
     Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: "som id here",
+    id: 4,
   },
   {
     hebrew: "דדדדד",
     arabic: "ددددد",
     Hspelling: "beit sefer", // Hebrew pronounciation in english letters
     Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: "som id here",
+    id: 5,
   },
   {
     hebrew: "הההההה ",
     arabic: "هههه",
     Hspelling: "beit sefer", // Hebrew pronounciation in english letters
     Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: "som id here",
+    id: 6,
   },
   {
     hebrew: "ווווו",
     arabic: "ووووو",
     Hspelling: "beit sefer", // Hebrew pronounciation in english letters
     Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: "som id here",
+    id:7,
   },
 ];
