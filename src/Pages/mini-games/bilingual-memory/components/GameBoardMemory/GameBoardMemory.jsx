42554eeaafe94341dd99d8bde324eba5f6fcaac7
Snork712 <snir712@gmail.com>
2022-06-30 02:30:56 +0300

    creating route

--- src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
+++ src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
@@ -5,7 +5,7 @@ import { shuffleRandomly } from "../../../../../utils/utils";
 import TurnPanel from "../TurnPanel/TurnPanel";
 import gotchya from "../../../../../api/gotchyaApi";
 
-const GameBoardMemory = () => {
+const GameBoardMemory = (props) => {
   const [cards, setCards] = useState([]);
   const [openCards, setOpenCards] = useState([]);
   const [clearedCards, setClearedCards] = useState({});
@@ -13,52 +13,58 @@ const GameBoardMemory = () => {
   const [pairsLeft, setPairsLeft] = useState(6);
   const [turn, setTurn] = useState(true);
   const timeout = useRef(null);
-  
 
-  useEffect(()=>{
-    getWords()
-    randomizeAndFill()
-  },[])
+  useEffect(() => {
+    if (pairsLeft === 0) {
+      props.isDone(true);
+    }
+  }, [pairsLeft]);
+
+  useEffect(() => {
+    getWords();
+    randomizeAndFill();
+  }, []);
 
   const getWords = async () => {
-    let {data} =  await gotchya.get('/wordBank');
-    console.log(data);
+    let { data } = await gotchya.get("/wordBank");
     return data;
-   }
+  };
 
   const randomizeAndFill = async () => {
-        // randomly choose 6 word objects from data to fill 12 tiles
-  let arrayOfWordObjects = [];
-  let i = 0;
-  let rawData = await getWords();
-  while (i < 6) {
-    let randomIndex = Math.floor(Math.random() * rawData.length);
-    arrayOfWordObjects.push(rawData[randomIndex]);
-    i++;
-  }
-  // make array of {word & ID} pairs from hebrew and arabic and id
-  let wordsArray = []
-  arrayOfWordObjects.forEach((word, i) => {
-      wordsArray.push({ word: word.hebrew, id: i},
-                      { word: word.arabic, id: i, isArabic: true })
+    // randomly choose 6 word objects from data to fill 12 tiles
+    let arrayOfWordObjects = [];
+    let i = 0;
+    let rawData = await getWords();
+    while (i < 6) {
+      let randomIndex = Math.floor(Math.random() * rawData.length);
+      arrayOfWordObjects.push(rawData[randomIndex]);
+      i++;
+    }
+    // make array of {word & ID} pairs from hebrew and arabic and id
+    let wordsArray = [];
+    arrayOfWordObjects.forEach((word, i) => {
+      wordsArray.push(
+        { word: word.hebrew, id: i },
+        { word: word.arabic, id: i, isArabic: true }
+      );
     });
     let shuffled = shuffleRandomly(wordsArray);
-  setCards(shuffled);
-  }
+    setCards(shuffled);
+  };
 
   // Check if both the words have same id. If so, mark them inactive
   const evaluate = () => {
     const [first, second] = openCards;
     if (cards[first].id === cards[second].id) {
-      if(pairsLeft===0){
-        alert('Good Job!');   // winning scenario here
-        return
+      if (pairsLeft === 0) {
+        alert("Good Job!"); // winning scenario here
+        return;
       }
-      setPairsLeft(prev => prev-1)
+      setPairsLeft((prev) => prev - 1);
       setClearedCards((prev) => ({ ...prev, [cards[first].id]: true }));
       setOpenCards([]);
     }
-    // Flip cards after a 500ms 
+    // Flip cards after a 500ms
     timeout.current = setTimeout(() => {
       setOpenCards([]);
     }, 500);
@@ -70,7 +76,6 @@ const GameBoardMemory = () => {
       setOpenCards((prev) => [...prev, index]);
       // increase the moves once we opened a pair
       setMoves((moves) => moves + 1);
-      
     } else {
       // If two cards are already open, cancel timeout set for flipping cards back
       clearTimeout(timeout.current);
@@ -103,28 +108,20 @@ const GameBoardMemory = () => {
             isArabic={word.isArabic || ""}
             onClick={handleCardClick}
             isInactive={checkIsInactive(word)}
-              isFlipped={checkIsFlipped(i)}
+            isFlipped={checkIsFlipped(i)}
           />
         </div>
       );
     });
   };
 
-
-
-
-  return( 
+  return (
     <>
-    <TurnPanel isPlaying={turn} />
-  <div className="GameBoardMemory">{drawBoard(cards)}</div>
-  <TurnPanel buttom={true} isPlaying={!turn} />
-  </>
-  )
-
-
+      <TurnPanel isPlaying={turn} />
+      <div className="GameBoardMemory">{drawBoard(cards)}</div>
+      <TurnPanel buttom={true} isPlaying={!turn} />
+    </>
+  );
 };
 
-
-
 export default GameBoardMemory;
-
