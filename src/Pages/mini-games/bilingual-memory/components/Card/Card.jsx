919b1dc9fe2bd4d0aa56be4f438f51a7ea8a3c02
Sobhi Mansour <sobhi@Sobhis-MBP.lan>
2022-06-29 16:06:14 +0300

    memory-game logic

--- src/Pages/mini-games/bilingual-memory/components/Card/Card.jsx
+++ src/Pages/mini-games/bilingual-memory/components/Card/Card.jsx
@@ -2,24 +2,22 @@ import "./Card.css";
 import React, { useEffect, useState } from "react";
 
 const Card = (props) => {
-  const [hidden, setHidden] = useState();
-  const [open, setOpen] = useState(false);
   return (
-
-    <div id="CardParent" onClick={()=>{
-      props.onClick(props.id); 
-      props.flips<2 ? setOpen(true) : setOpen(false);
-      }}>
-        
-    {  open  ? 
-    <div  className="back">
-        <p  className={ props.isArabic ? `arabicCard` : null }>{props.text}</p>
-      </div> 
-       :  
-       <div className="front">front</div>
-    
-}
-</div>
-)}
+    <div className={props.isInactive ? "hidden" : ''}
+    id="CardParent"
+    onClick={() => {
+      props.onClick(props.index);
+      }}
+    >
+      {props.isFlipped ? (
+        <div className="back">
+          <p className={props.isArabic ? `arabicCard` : null}>{props.text}</p>
+        </div>
+      ) : (
+        <div className="front"></div>
+      )}
+    </div>
+  );
+};
 
 export default Card;
--- src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
+++ src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
@@ -1,111 +1,95 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Card from "../Card/Card";
 import "./GameboardMemory.css";
 import { shuffleRandomly } from "../../../../../utils/utils";
 
 const GameBoardMemory = () => {
-  const [currentId, setCurrentId] = useState();
-  const [hidden, setHidden] = useState("hidden");
-  // const [firstFlip, setFirstFlip] = useState(true);
-  const [flips, setFlips] = useState(0);
-  const [wordsArrayState, setWordsArrayState] = useState(0);
+  const [cards, setCards] = useState([]);
+  const [openCards, setOpenCards] = useState([]);
+  const [clearedCards, setClearedCards] = useState({});
+  const [moves, setMoves] = useState(0);
+  const timeout = useRef(null);
 
-
-  // randomly choose 6 word objects from data to fill 12 tiles
+  useEffect(()=>{
+    // randomly choose 6 word objects from data to fill 12 tiles
   let arrayOfWordObjects = [];
   let i = 0;
   while (i < 6) {
-    let randomIndex = Math.floor(Math.random() * myWords.length);
-    arrayOfWordObjects.push(myWords[randomIndex]);
+    // let randomIndex = Math.floor(Math.random() * myWords.length);
+    // arrayOfWordObjects.push(myWords[randomIndex]);
+    arrayOfWordObjects.push(myWords[i]);
     i++;
   }
-
-  //make array of {word & ID} pairs from hebrew and arabic and id
+  // make array of {word & ID} pairs from hebrew and arabic and id
   let wordsArray = []
   arrayOfWordObjects.forEach((word) => {
       wordsArray.push({ word: word.hebrew, id: word.id },
                       { word: word.arabic, id: word.id, isArabic: true })
-                    });
-                    setWordsArrayState(...wordsArray)
+    });
+  setCards(wordsArray);
+  },[])
 
-  let shuffledWords = shuffleRandomly(wordsArray);
+  // Check if both the words have same id. If so, mark them inactive
+  const evaluate = () => {
+    const [first, second] = openCards;
+    if (cards[first].id === cards[second].id) {
+      setClearedCards((prev) => ({ ...prev, [cards[first].id]: true }));
+      setOpenCards([]);
+      //show pic of word? 
+      return;
+    }
+    // Flip cards after a 500ms duration
+    timeout.current = setTimeout(() => {
+      setOpenCards([]);
+    }, 500);
+  };
 
+  const handleCardClick = (index) => {
+    // Have a maximum of 2 items in array at once.
+    if (openCards.length === 1) {
+      setOpenCards((prev) => [...prev, index]);
+      // increase the moves once we opened a pair
+      setMoves((moves) => moves + 1);
+    } else {
+      // If two cards are already open, we cancel timeout set for flipping cards back
+      clearTimeout(timeout.current);
+      setOpenCards([index]);
+    }
+  };
+
+  useEffect(() => {
+    if (openCards.length === 2) {
+      setTimeout(evaluate, 500);
+    }
+  }, [openCards]);
+
+  const checkIsFlipped = (index) => {
+    return openCards.includes(index);
+  };
+
+  const checkIsInactive = (card) => {
+    console.log(clearedCards[card.id]);
+    return Boolean(clearedCards[card.id]);
+  };
   const drawBoard = (words) => {
     return words.map((word, i) => {
       return (
         <div key={i}>
           <Card
-           text={word.word} 
-           id={word.id} 
-           isArabic={word.isArabic} 
-           onClick={handleTileClick}
-           flips={flips}
-            />
+            text={word.word}
+            id={word.id}
+            index={i}
+            isArabic={word.isArabic || ""}
+            onClick={handleCardClick}
+            isInactive={checkIsInactive(word)}
+              isFlipped={checkIsFlipped(i)}
+          />
         </div>
       );
     });
   };
-  const handleTileClick = (e) => {
-    if (flips<2) {
-      setFlips(prev => prev+1);
-       setCurrentId(e);
-       
-      //save current card to state and wait
-    } else {
-        //do matching logic
-        e === currentId ?  console.log('handleRightAnswer and removecards') 
-        : console.log('close both cards');
-        setFlips(0);
-    }
-  };
-  return <div className="GameBoardMemory">{drawBoard(shuffledWords)}</div>;
+
+  return <div className="GameBoardMemory">{drawBoard(cards)}</div>;
 };
 export default GameBoardMemory;
 
-
-
-
-const myWords = [
-  {
-    hebrew: "אאאא",
-    arabic: "ااااا",
-    Hspelling: "beit sefer", // Hebrew pronounciation in english letters
-    Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: 2,
-  },
-  {
-    hebrew: "בבבב",
-    arabic: "بببب",
-    Hspelling: "beit sefer", // Hebrew pronounciation in english letters
-    Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: 3,
-  },
-  {
-    hebrew: "גגגגג",
-    arabic: "ججججج",
-    Hspelling: "beit sefer", // Hebrew pronounciation in english letters
-    Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: 4,
-  },
-  {
-    hebrew: "דדדדד",
-    arabic: "ددددد",
-    Hspelling: "beit sefer", // Hebrew pronounciation in english letters
-    Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: 5,
-  },
-  {
-    hebrew: "הההההה ",
-    arabic: "هههه",
-    Hspelling: "beit sefer", // Hebrew pronounciation in english letters
-    Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id: 6,
-  },
-  {
-    hebrew: "ווווו",
-    arabic: "ووووو",
-    Hspelling: "beit sefer", // Hebrew pronounciation in english letters
-    Aspelling: "madrasa", // Arabic pronounciation in english letters,
-    id:7,
-  },
-];
