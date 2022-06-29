fb99f023ccbd267448ed794f0654eee144e55564
Sobhi Mansour <sobhi@Sobhis-MBP.lan>
2022-06-30 00:53:07 +0300

    added handle winning scenario

--- src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
+++ src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
@@ -10,6 +10,7 @@ const GameBoardMemory = () => {
   const [openCards, setOpenCards] = useState([]);
   const [clearedCards, setClearedCards] = useState({});
   const [moves, setMoves] = useState(0);
+  const [pairsLeft, setPairsLeft] = useState(6);
   const [turn, setTurn] = useState(true);
   const timeout = useRef(null);
   
@@ -20,8 +21,9 @@ const GameBoardMemory = () => {
   },[])
 
   const getWords = async () => {
-    let {data} =  await gotchya.get('/wordBank');
-    return data;
+    // let {data} =  await gotchya.get('/wordBank');
+    // return data;
+    return myWords;
    }
 
   const randomizeAndFill = async () => {
@@ -42,16 +44,21 @@ const GameBoardMemory = () => {
     });
     let shuffled = shuffleRandomly(wordsArray);
   setCards(shuffled);
-  console.log(shuffled);
   }
 
   // Check if both the words have same id. If so, mark them inactive
   const evaluate = () => {
     const [first, second] = openCards;
     if (cards[first].id === cards[second].id) {
+      if(pairsLeft===0){
+        alert('Good Job!');   // winning scenario here
+        return
+      }
+      setPairsLeft(prev => prev-1)
       setClearedCards((prev) => ({ ...prev, [cards[first].id]: true }));
       setOpenCards([]);
     }
+    console.log(pairsLeft);
 
     
     
@@ -125,3 +132,132 @@ const GameBoardMemory = () => {
 
 export default GameBoardMemory;
 
+
+
+const myWords = [{
+  "hebrew": "אריה",
+  "arabic": "اسد",
+  "Hspelling": "אסאד",
+  "Aspelling": "ارييه"
+},
+{
+  "hebrew": "חתול",
+  "arabic": "قط",
+  "Hspelling": "קיט",
+  "Aspelling": "حاتول"
+},
+{
+  "hebrew": "כסא",
+  "arabic": "كرسي",
+  "Hspelling": "קורסיי",
+  "Aspelling": "كسيه"
+},
+{
+  "hebrew": "אולם",
+  "arabic": "قاعه",
+  "Hspelling": "קאעא",
+  "Aspelling": "اولام"
+},
+{
+  "hebrew": "רכב",
+  "arabic": "سياره",
+  "Hspelling": "סייארא",
+  "Aspelling": "ريخيف"
+},
+{
+  "hebrew": "עץ",
+  "arabic": "شجره",
+  "Hspelling": "שאגארא",
+  "Aspelling": "عيتس"
+},
+{
+  "hebrew": "שולחן",
+  "arabic": "طاوله",
+  "Hspelling": "טאוולא",
+  "Aspelling": "شولحان"
+},
+{
+  "hebrew": "אריה",
+  "arabic": "اسد",
+  "Hspelling": "אסאד",
+  "Aspelling": "ارييه"
+},
+{
+  "hebrew": "ציפור",
+  "arabic": "عصفور",
+  "Hspelling": "עספור",
+  "Aspelling": "تسيبور"
+},
+{
+  "hebrew": "בניין",
+  "arabic": "عماره",
+  "Hspelling": "עמארה",
+  "Aspelling": "بنيان"
+},
+{
+  "hebrew": "ענן",
+  "arabic": "غيمه",
+  "Hspelling": "גיימה",
+  "Aspelling": "عنان"
+},
+{
+  "hebrew": "שלום",
+  "arabic": "سلام",
+  "Hspelling": "סלאם",
+  "Aspelling": "شالوم"
+},
+{
+  "hebrew": "חברים",
+  "arabic": "اصحاب",
+  "Hspelling": "אסחאב",
+  "Aspelling": "حابيريم"
+},
+{
+  "hebrew": "אהבה",
+  "arabic": "حب",
+  "Hspelling": "חוב",
+  "Aspelling": "اهافا"
+},
+{
+  "hebrew": "מטוס",
+  "arabic": "طياره",
+  "Hspelling": "טיארא",
+  "Aspelling": "ماطوس"
+},
+{
+  "hebrew": "גבולות",
+  "arabic": "حدود",
+  "Hspelling": "חדוד",
+  "Aspelling": "جبولوت"
+},
+{
+  "hebrew": "שמן זית",
+  "arabic": "زيت زيتون",
+  "Hspelling": "זית זייתון",
+  "Aspelling": "شيمين زاييت"
+},
+{
+  "hebrew": "כלב",
+  "arabic": "كلب",
+  "Hspelling": "כאלב",
+  "Aspelling": "كيليب"
+},
+{
+  "hebrew": "מלחמה",
+  "arabic": "حرب",
+  "Hspelling": "חארב",
+  "Aspelling": "ميلحماه"
+},
+{
+  "hebrew": "עם",
+  "arabic": "شعب",
+  "Hspelling": "שאעב",
+  "Aspelling": "عام"
+},
+{
+  "hebrew": "נשר",
+  "arabic": "نسر",
+  "Hspelling": "ניסר",
+  "Aspelling": "نيشير"
+}
+]
